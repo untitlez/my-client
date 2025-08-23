9b8181b4364cb1ca5d89299db8ab4c209b32f26a
untitlez <tleza_championship2@hotmail.com>
2025-08-24 05:18:38 +0700

    fix : error

--- app/(pages)/office/page.tsx
+++ app/(pages)/office/page.tsx
@@ -1,11 +1,9 @@
-import { fetchLessonPlan, fetchRole } from "@/lib/fetch";
+import { fetchLessonPlan } from "@/lib/fetch";
 
 import { ContentTable } from "@/components/office/content/content-table";
 
 export default async function OfficePage() {
   const data = await fetchLessonPlan();
-  const role = await fetchRole()
-  console.log('role', role)
 
   return (
     <div className="flex flex-col items-center">
